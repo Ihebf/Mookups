0b1e9d19a9d46f5febd54b656f87475e1d971131
Ihebf <iheb_farhat@hotmail.com>
2020-10-05 13:35:59 +0100

    updated

--- src/components/HeaderFixed.js
+++ src/components/HeaderFixed.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, TextInput } from 'react-native';
 import {FontAwesome, MaterialIcons}  from '@expo/vector-icons';
 import Colors from '../styles/Color'
 import DialogInput from '../components/DialogInput';
+import { AntDesign } from '@expo/vector-icons'; 
 
 const HeaderFixed = (props) => {
+   const [searchActive, setSearchActive] = React.useState(false);
+
     return (
         <View style={styles.wrapper}>
             <View style={styles.left}>
-              <TouchableOpacity style={styles.button} onPress={()=>{<DialogInput/>}}>
+              <TouchableOpacity style={styles.button} onPress={()=>setSearchActive(!searchActive)}>
                 <FontAwesome name='search' size={20} color={Colors.gray} style={{fontWeight:'900'}}/>
               </TouchableOpacity>
             </View>
             <View style={styles.mid}>
-              <Text style={styles.text}> 15.5$ </Text>
+            {
+              searchActive? 
+              <View style={styles.searchView}>
+                <TextInput 
+                  style={styles.textInput}
+                  onChangeText={props.textInputOnChangeText}
+                  value={props.textInputValue}
+                />
+                <TouchableOpacity style={styles.submit} onPress={()=>{setSearchActive(!searchActive)}}>
+                  <AntDesign name="enter" size={30} color="white" />
+                </TouchableOpacity>
+              </View> 
+            : 
+              <View style={styles.textZone}>
+                <Text style={styles.text}>15.5$</Text>
+              </View>
+            }
             </View>
             <View style={styles.right}>
               <TouchableOpacity style={styles.button} onPress={props.gpsBtn}>
@@ -32,36 +51,64 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         alignContent:'space-between',
         alignItems:'center',
+        justifyContent:'center',
+        alignSelf:'center'
     },
     left:{
-        flex:0.3,
+        flex:0.1,
         alignItems:'flex-start',
         marginLeft:10,
     },
     button:{
-        padding:9,
+        padding:7,
         backgroundColor: Colors.white,
         borderRadius:44/2,
     },
     mid:{
-        flex:0.4,
+        flex:0.7,
         display:'flex',
+        flexWrap:'wrap',
+        justifyContent:'center',
         alignItems:'center',
         alignContent:'center',
-        justifyContent: 'center',
-        backgroundColor:Colors.green,
-        borderRadius:100,
-        height:25,
+        paddingLeft:20
+
     },
     right:{
-        flex:0.3,
+        flex:0.2,
         display:'flex',
         alignItems:'flex-end',
-        paddingRight:10
+        paddingRight:10,
     },
     text:{
       fontWeight:'bold',
       fontSize:14,
       color:Colors.white
+    },
+    searchView:{
+      flex:1,
+      flexDirection:'row',
+      alignContent:'center',
+      alignItems:'center',
+    },
+    textInput:{
+      width:200,
+      height:40,
+      borderRadius:5,
+      borderWidth:1,
+      borderColor:Colors.black,
+      backgroundColor:Colors.white,
+      margin:5
+    },
+    submit:{
+      backgroundColor:Colors.black
+    },
+    textZone:{
+      backgroundColor:Colors.green,
+      height:25,
+      width:100,
+      alignItems:'center',
+      justifyContent:'center',
+      borderRadius:100
     }
 });
--- src/styles/Color.js
+++ src/styles/Color.js
@@ -1,5 +1,6 @@
 export default {
     white:"#fff",
+    black:"#000",
     green:"#28A893",
     gray:'#747474',
     lightGreen:'#9DCEC6',
